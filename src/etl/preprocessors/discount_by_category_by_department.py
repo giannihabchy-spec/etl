24f1014f1e78801af9aca1cb6beb5c6904305761
Gianni <giannihabchy@gmail.com>
2026-02-06 11:27:12 +0200

    preprocess / discount by items

--- src/etl/preprocessors/discount_by_category_by_department.py
+++ src/etl/preprocessors/discount_by_category_by_department.py
@@ -7,7 +7,7 @@ from etl.utils import make_columns_numeric
 def preprocess(path):
     data = read(path)
     data = keep_cols_by_index(data,[1,4])
-    data.columns=['Category','Total']
+    data.columns = ['Category','Total']
     data = drop_na_by_name(data,['Category'])
     data = make_columns_numeric(data,['Total'])
     return data
---
+++ src/etl/preprocessors/discount_by_items.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from etl.utils import read
+from etl.utils import keep_cols_by_index
+from etl.utils import drop_na_by_name
+from etl.utils import make_columns_numeric
+
+def preprocess(path):
+    data = read(path)
+    data = keep_cols_by_index(data,[0,5,7])
+    data.columns = ['Check','Description','QTY']
+    data = drop_na_by_name(data,['description','qty'])
+    data = make_columns_numeric(data,['QTY'])
+    return data
