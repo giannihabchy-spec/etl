baeb07dfb00a85602c29c1f643ac4fa1cbac8e64
Gianni <giannihabchy@gmail.com>
2026-02-10 11:02:13 +0200

    preprocess / summary of sales by customer by item

---
+++ src/etl/preprocessors/summary_of_sales_by_customer_by_item.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from etl.utils import read
+from etl.utils import keep_cols_by_index
+from etl.utils import remove_repeated_headers
+from etl.utils import drop_na_by_name
+from etl.utils import make_columns_numeric
+
+
+def preprocess(path):
+    data = read(path)
+    data = keep_cols_by_index(data,[0,3,4,13])
+    data.columns = ['Product Code', 'Description', 'Qty', 'Total Price']
+    id = data[data['Product Code'] == 'Product Code'].index[0] # Keep only the rows after the first Header
+    data = data.iloc[id:].copy()
+    data = remove_repeated_headers(data,'Product Code')
+
+    # Create 'Customer' from 'Product Code'
+    cust_ids = data[data['Product Code'].str.contains('Customer Name: ',na=False)].index
+    data.loc[cust_ids,'Customer'] = data.loc[cust_ids,'Product Code'].str.replace('Customer Name: ','',regex = False)
+    data['Customer'] = data['Customer'].ffill()
+
+    # Create 'Location' from 'Product Code'
+    loc_ids = data[data['Product Code'].str.contains('Location: ',na=False)].index
+    data.loc[loc_ids,'Location'] = data.loc[loc_ids,'Product Code'].str.replace('Location: ','',regex = False)
+    data['Location'] = data['Location'].ffill()
+
+    # 'Date' is 'Product Code' converted to type date
+    data['Date'] = pd.to_datetime(data['Product Code'],errors='coerce').dt.date
+    data['Date'] = data['Date'].ffill()
+
+    # Create 'Invoice' from 'Product Code'
+    inv_ids = data[data['Product Code'].str.contains('Invoice Number: ', na = False)].index
+    data.loc[inv_ids,'Invoice'] = data.loc[inv_ids,'Product Code'].str.replace('Invoice Number: ', '', regex = False)
+    data['Invoice'] = data['Invoice'].ffill()
+
+    data = drop_na_by_name(data,['Description'])
+    data = data.drop(columns='Product Code').copy()
+    data = make_columns_numeric(data,['Qty','Total Price'])
+    return data
