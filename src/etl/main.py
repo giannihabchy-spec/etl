25e6da7e5ac259f297244ca822f25fe36056ac7e
Gianni <giannihabchy@gmail.com>
2026-02-10 20:16:52 +0200

    main / no default style warning

--- src/etl/main.py
+++ src/etl/main.py
@@ -1,2 +1,10 @@
+import warnings
+
+warnings.filterwarnings(
+    "ignore",
+    message="Workbook contains no default style*",
+    category=UserWarning,
+)
+
 def run():
     print("ETL starts here.")
