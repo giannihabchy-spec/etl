fbad1694bd2e1bce6cfbd9223668d17f7cccb321
Gianni <giannihabchy@gmail.com>
2026-02-10 18:40:25 +0200

    preprocess / sales by items / dtypes

--- src/etl/preprocessors/sales_by_items.py
+++ src/etl/preprocessors/sales_by_items.py
@@ -3,6 +3,7 @@ from etl.utils import read
 from etl.utils import keep_cols_by_index
 from etl.utils import drop_na_by_name
 from etl.utils import remove_repeated_headers
+from etl.utils import make_columns_numeric
 
 
 def preprocess(path):
@@ -11,4 +12,5 @@ def preprocess(path):
     data.columns = ['Description','Qty','Total Amount']
     data = drop_na_by_name(data,['Qty'])
     data = remove_repeated_headers(data,'Qty')
+    data = make_columns_numeric(data,['Qty','Total Amount'])
     return data
