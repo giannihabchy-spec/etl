ee7550876404e7e570cb203d9c6db9320d40d785
Gianni <giannihabchy@gmail.com>
2026-02-13 16:46:20 +0200

    preprocess / programming summary sales SP

---
+++ src/etl/preprocessors/programming_summary_sales_SP.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from etl.utils import read
+from etl.utils import keep_cols_by_index
+from etl.utils import drop_na_by_name
+from etl.utils import remove_repeated_headers
+from etl.utils import make_columns_numeric
+
+
+def preprocess(path):
+    data = read(path)
+    data = keep_cols_by_index(data,[1,4])
+    data.columns = ['Description','Price']
+    data = drop_na_by_name(data,['Description'])
+    data = remove_repeated_headers(data,'Description')
+    data = make_columns_numeric(data,['Price'])
+    return data
