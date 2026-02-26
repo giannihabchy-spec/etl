bff1231f5c7fd75d86e888a800c945cdae351db3
Gianni <giannihabchy@gmail.com>
2026-02-26 15:03:25 +0200

    preprocess / discount by description by server

---
+++ src/etl/preprocessors/local/discount_by_description_by_server.py
@@ -0,0 +1,20 @@
+# In orchestrator: discount by description by employee
+
+from etl.utils import read
+from etl.utils import keep_cols_by_index
+from etl.utils import remove_repeated_headers
+from etl.utils import drop_rows
+from etl.utils import make_columns_numeric
+from etl.utils import drop_na_by_name
+
+
+def preprocess(path):
+    data = read(path)
+    data = keep_cols_by_index(data,[0,7,8])
+    data.columns = ['Check', 'Discount', 'Amount']
+    data = remove_repeated_headers(data,'Discount')
+    data = drop_rows(data,'Discount')
+    data = drop_na_by_name(data,['Check','Discount','Amount'])
+    data = make_columns_numeric(data,['Discount','Amount'])
+    data['Discount_Percentage'] = data['Discount'] / data['Amount']
+    return data
