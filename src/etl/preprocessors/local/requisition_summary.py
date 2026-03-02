197df4adcb4b7ab8276ff7d2ae518d2028a508bc
Gianni <giannihabchy@gmail.com>
2026-03-02 16:24:10 +0200

    preprocess / requisition summary

---
+++ src/etl/preprocessors/local/requisition_summary.py
@@ -0,0 +1,20 @@
+# all to all
+
+from etl.utils import read
+from etl.utils import keep_cols_by_index
+from etl.utils import remove_repeated_headers
+from etl.utils import make_columns_date
+from etl.utils import make_columns_numeric
+
+
+def preprocess(path):
+    data = read(path)
+    data = keep_cols_by_index(data,[0, 2, 3, 4, 5, 6, 8])
+    data.columns = ['From Location','From Branch','To Location','To Branch','Date','Product','Qty']
+    data = data.dropna(how='any')
+    data = remove_repeated_headers(data,'From Location')
+    data = make_columns_numeric(data,['Qty'])
+    data = make_columns_date(data,['Date'])
+    cols = ['Product','From Location','From Branch','To Location','To Branch','Qty','Date']
+    data = data[cols]
+    return data
