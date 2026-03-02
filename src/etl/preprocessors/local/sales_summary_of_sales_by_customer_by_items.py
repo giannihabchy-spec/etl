c8c2bbfa260bd38f093702197d9102c211bc511a
Gianni <giannihabchy@gmail.com>
2026-03-02 12:03:02 +0200

    preprocess / sales summary of sales by customer by items

---
+++ src/etl/preprocessors/local/sales_summary_of_sales_by_customer_by_items.py
@@ -0,0 +1,15 @@
+from etl.utils import read
+from etl.utils import keep_cols_by_index
+from etl.utils import remove_repeated_headers
+from etl.utils import drop_na_by_name
+from etl.utils import make_columns_numeric
+
+
+def preprocess(path):
+    data = read(path)
+    data = keep_cols_by_index(data,[3,7])
+    data.columns = ['Product Description', 'Qty']
+    data = remove_repeated_headers(data,'Qty')
+    data = drop_na_by_name(data,['Product Description','Qty'])
+    data = make_columns_numeric(data,['Qty'])
+    return data
