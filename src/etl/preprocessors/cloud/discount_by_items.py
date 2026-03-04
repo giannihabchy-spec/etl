5ffe8dd2c7d85086b5856d78e9785608c468dba0
Gianni <giannihabchy@gmail.com>
2026-03-04 11:46:38 +0200

    omega fix / (cloud) discount by items

--- src/etl/preprocessors/cloud/discount_by_items.py
+++ src/etl/preprocessors/cloud/discount_by_items.py
@@ -6,9 +6,10 @@ from etl.utils import make_columns_numeric
 
 def preprocess(path):
     data = read(path)
-    data = keep_cols_by_index(data,[0,5,7])
+    data = keep_cols_by_index(data,[0,3,4])
     data.columns = ['Check','Description','QTY']
-    data = drop_na_by_name(data,['description','qty'])
-    data = make_columns_numeric(data,['QTY'])
+    data = drop_na_by_name(data,['Description','QTY'])
+    data = make_columns_numeric(data,['QTY'], er='coerce')
+    data = drop_na_by_name(data,['QTY'])
     data = data.reset_index(drop = True)
     return data
