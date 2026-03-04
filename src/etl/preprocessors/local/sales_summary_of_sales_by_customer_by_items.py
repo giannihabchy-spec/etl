c78a24f17f610b90a28dd5c439647127ba755d21
Gianni <giannihabchy@gmail.com>
2026-03-04 18:18:15 +0200

    add cols / (cloud) sales__summary of sales by cust by items

--- src/etl/preprocessors/local/sales_summary_of_sales_by_customer_by_items.py
+++ src/etl/preprocessors/local/sales_summary_of_sales_by_customer_by_items.py
@@ -3,13 +3,31 @@ from etl.utils import keep_cols_by_index
 from etl.utils import remove_repeated_headers
 from etl.utils import drop_na_by_name
 from etl.utils import make_columns_numeric
-
+from etl.utils import make_columns_date
 
 def preprocess(path):
     data = read(path)
-    data = keep_cols_by_index(data,[3,7])
-    data.columns = ['Product Description', 'Qty']
-    data = remove_repeated_headers(data,'Qty')
-    data = drop_na_by_name(data,['Product Description','Qty'])
-    data = make_columns_numeric(data,['Qty'])
+    data = keep_cols_by_index(data,[0,2,3,7,10])
+    data.columns = ['Code', 'Info', 'Desc', 'Qty', 'Amount']
+
+    cust_ids = data[data['Code'].str.contains('Customer Name :', na = False)].index
+    data.loc[cust_ids,'Customer'] = data.loc[cust_ids,'Info']
+    data['Customer'] = data['Customer'].ffill()
+
+    loc_ids = data[data['Code'].str.contains('LOCATION NAME:', na = False)].index
+    data.loc[loc_ids,'Location'] = data.loc[loc_ids,'Info']
+    data['Location'] = data['Location'].ffill()
+
+    date_ids = data[data['Code'].str.contains('Sales Date  :', na = False)].index
+    data.loc[date_ids,'Date'] = data.loc[date_ids,'Info']
+    data['Date'] = data['Date'].ffill()
+
+    inv_ids = data[data['Code'].str.contains('Invoice Number :', na = False)].index
+    data.loc[inv_ids,'Invoice'] = data.loc[inv_ids,'Info']
+    data['Invoice'] = data['Invoice'].ffill()
+    data = data.drop(columns=['Info','Code'])
+    data = data.dropna(how='any')
+    data.columns = ['Description','Qty','Total Price','Customer','Location','Date','Invoice']
+    data = make_columns_numeric(data,['Qty','Total Price'])
+    data = make_columns_date(data,['Date'])
     return data
