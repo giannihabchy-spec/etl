9254da8a3bfb3b6ce211f94ffe1bfa1ab6eaab6f
Gianni <giannihabchy@gmail.com>
2026-03-05 13:22:48 +0200

    add cols / (local) sales wastage items

--- src/etl/preprocessors/local/sales_wastage_items.py
+++ src/etl/preprocessors/local/sales_wastage_items.py
@@ -1,20 +1,23 @@
+# by branch
 from etl.utils import read
 from etl.utils import keep_cols_by_index
 from etl.utils import drop_rows
 from etl.utils import drop_na_by_name
-from etl.utils import remove_repeated_headers
 from etl.utils import make_columns_numeric
+from etl.utils import make_columns_date
 
 
 def preprocess(path):
     data = read(path)
-    data = keep_cols_by_index(data,[0,4,6,7])
-    data.columns = ['Product Description','Total by item', 'Qty', 'Remark']
-    data = drop_rows(data,'Total by item',value='Total by Item:')
+    data = keep_cols_by_index(data,[0,1,3,6,7,8])
+    data.columns = ['Product Description','loc','Date', 'Qty', 'Remark','Unit Cost']
+    ids = data[data['loc'].str.contains('Location Name :', na = False)].index
+    data.loc[ids,'Location'] = data.loc[ids,'Date']
+    data['Location'] = data['Location'].ffill()
     data['Product Description'] = data['Product Description'].ffill()
-    data = drop_na_by_name(data,['Qty'])
-    data = remove_repeated_headers(data,'Product Description')
-    cols = ['Product Description', 'Qty', 'Remark']
-    data = data[cols]
-    data = make_columns_numeric(data,['Qty'])
+    data = make_columns_date(data,['Date'],er='coerce')
+    data = drop_na_by_name(data,['Date'])
+    cols = ['Product Description', 'Qty', 'Remark','Date','Unit Cost','Location']
+    data =  data[cols].copy()
+    data = make_columns_numeric(data,['Qty','Unit Cost'])
     return data
