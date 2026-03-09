497a5e9222b1b6e87ec14c619dcc3144b949a205
Gianni <giannihabchy@gmail.com>
2026-03-09 11:29:30 +0200

    fix / (cloud) requisition summary

--- src/etl/preprocessors/cloud/requisition_summary.py
+++ src/etl/preprocessors/cloud/requisition_summary.py
@@ -10,18 +10,15 @@ from etl.utils import make_columns_date, make_columns_numeric
 
 def preprocess(path):
     data = read(path)
-    data = keep_cols_by_index(data,[0,1,2,3,4,6,])
-    data.columns = ['From Branch', 'To Branch', 'To Location', 'Date', 'Product', 'Qty']
-    data = remove_repeated_headers(data,'Product')
-
-    # Creating 'From Location'
-    ids = data[data['From Branch'].str.contains('From Location: ', na = False)].index
-    data.loc[ids,'From Location'] = data.loc[ids,'From Branch'].str.replace('From Location: ', '', regex = False)
-    data['From Location'] = data['From Location'].ffill()
+    data = keep_cols_by_index(data,[0,1,2,3,4,6])
+    data.columns = ['From Location', 'To Branch', 'To Location', 'Date', 'Product', 'Qty'] 
+    ids = data[data['From Location'].str.startswith('Branch: ', na=False)].index
+    data.loc[ids,'From Branch'] = data.loc[ids,'From Location'].str.replace('Branch: ','',regex=False)
+    data['From Branch'] = data['From Branch'].ffill()
     data = drop_na_by_name(data,['Product'])
-
+    data = remove_repeated_headers(data,'Product')
+    data = make_columns_date(data,['Date'])
+    data = make_columns_numeric(data,['Qty'])
     cols = ['Product','From Location','From Branch','To Location','To Branch','Qty','Date']
     data = data[cols]
-    data = make_columns_numeric(data,['Qty'])
-    data = make_columns_date(data,['Date'])
     return data
