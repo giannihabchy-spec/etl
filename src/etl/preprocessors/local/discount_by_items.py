571352aad42dd3e7428561366935be052fc26587
Gianni <giannihabchy@gmail.com>
2026-03-11 10:26:39 +0200

    add clean_check to preprocessors

--- src/etl/preprocessors/local/discount_by_items.py
+++ src/etl/preprocessors/local/discount_by_items.py
@@ -2,6 +2,7 @@ from etl.utils import read
 from etl.utils import keep_cols_by_index
 from etl.utils import drop_na_by_name
 from etl.utils import make_columns_numeric
+from etl.utils import clean_check
 
 
 def preprocess(path):
@@ -9,5 +10,6 @@ def preprocess(path):
     data = keep_cols_by_index(data,[3,5,7])
     data.columns = ['Check','Description','QTY']
     data = drop_na_by_name(data,['Check'])
+    data = clean_check(data,['Check'])
     data = make_columns_numeric(data,['QTY'])
     return data
