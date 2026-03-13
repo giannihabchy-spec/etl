4c5266aeeb912b02ac7f898e4781b76c1d77ab42
Gianni <giannihabchy@gmail.com>
2026-03-13 14:09:37 +0200

    preprocess / (cloud) inventory items ingredients qtp

---
+++ src/etl/preprocessors/cloud/inventory_items_ingredients_qtp.py
@@ -0,0 +1,25 @@
+from etl.utils import read
+from etl.utils import keep_cols_by_index
+from etl.utils import drop_na_by_name
+from etl.utils import remove_repeated_headers
+from etl.utils import make_columns_numeric
+
+
+def preprocess(path):
+    data = read(path)
+    data = keep_cols_by_index(data,[1,2,4])
+    data.columns = ['Production Name', 'Product Description', 'Qty']
+    ids = data[data['Production Name'].notna()].index
+    data.loc[ids,'Production Name'] = data.loc[ids,'Production Name'].str.replace('Product Code: ','',regex=False)
+    data['Production Name'] = data['Production Name'].ffill()
+    data.loc[ids,'Qty'] = data.loc[ids,'Qty'].str.replace('Ingredients to prepare ','',regex=False)
+    data.loc[ids,'to prepare'] = data.loc[ids,'Qty'].str.split().apply(lambda x: x[:2])
+    data.loc[ids,'Qty to be Prepared'] = data.loc[ids,'to prepare'].apply(lambda x: x[0])
+    data.loc[ids,'Prepared Unit'] = data.loc[ids,'to prepare'].apply(lambda x: x[1])
+    data[['Qty to be Prepared','Prepared Unit']] = data[['Qty to be Prepared','Prepared Unit']].ffill()
+    data = remove_repeated_headers(data,'Qty')
+    data = drop_na_by_name(data,['Product Description','Qty'])
+    data = make_columns_numeric(data,['Qty','Qty to be Prepared'])
+    cols = ['Production Name', 'Product Description', 'Qty','Qty to be Prepared', 'Prepared Unit']
+    data = data[cols].copy()
+    return data
