304d9ac90fd8d1a0c3032963ce5b0fd25ed7f158
Gianni <giannihabchy@gmail.com>
2026-04-13 11:47:55 +0300

    preprocess / inv items ingredients (local) / remove code

--- src/etl/preprocessors/local/inventory_items_ingredients_qtp.py
+++ src/etl/preprocessors/local/inventory_items_ingredients_qtp.py
@@ -14,9 +14,11 @@ def preprocess(path):
     data['get_ids'] = data['Qty']
     data = make_columns_numeric(data,['get_ids'], er='coerce')
     ids = data[data['get_ids'].isna()].index
-    data.loc[ids,'Production Name'] = data.loc[ids,'Product Description']
-    data['Production Name'] = data['Production Name'].ffill()
+    # data.loc[ids,'Production Name'] = data.loc[ids,'Product Description']
+    # data['Production Name'] = data['Production Name'].ffill()
     data.loc[ids,'Qty'] = data.loc[ids,'Qty'].str.replace('Ingredients to prepare ','',regex=False)
+    data.loc[ids,'Production Name'] = data.loc[ids,'Qty'].str.split().apply(lambda x: ' '.join(x[3:]))
+    data['Production Name'] = data['Production Name'].ffill()
     data.loc[ids,'to prepare'] = data.loc[ids,'Qty'].str.split().apply(lambda x: x[:2])
     data.loc[ids,'Qty to be Prepared'] = data.loc[ids,'to prepare'].apply(lambda x: x[0])
     data.loc[ids,'Prepared Unit'] = data.loc[ids,'to prepare'].apply(lambda x: x[1])
